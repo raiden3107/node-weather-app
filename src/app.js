0e74f5efb8dd4fe5d4db0bc529583baeda17cdd4
raiden3107 <hasan.nadeem97@hotmail.com>
2020-01-02 22:46:14 +0500

    setup heroku

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const app = express()
 const publicDir = path.join(__dirname,'../public')
 const viewPath = path.join(__dirname,'../template/views')
 const partialPath = path.join(__dirname,'../template/partials')
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'hbs')
 app.set('views', viewPath)
@@ -70,6 +71,6 @@ app.get('*', (req,res) => {
     })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up!')
 })
